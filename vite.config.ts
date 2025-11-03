945be08db276918d837874a528985f9d3171723a
Sasoun Torossian <sasoun.torossian@protonmail.com>
2025-11-03 11:37:52 +0000

    feat: add repo as vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://sasountorossian.github.io/Todo-V2/",
+  base: "/Todo-V2/",
 });
